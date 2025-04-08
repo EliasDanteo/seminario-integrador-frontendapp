3e02e408cd828c4d2ebab76cd185b6c44b681986
BorsatoMilton <borsatomilton@gmail.com>
2025-04-07 21:49:53 -0300

    feat: Inicio de componente-list + dialog de recordatorios

---
+++ src/app/cuu-casos/casos-information-dialog/recordatorios-caso/recordatorios-dialog/recordatorios-dialog.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-recordatorios-dialog',
+  standalone: true,
+  imports: [],
+  templateUrl: './recordatorios-dialog.component.html',
+  styleUrl: './recordatorios-dialog.component.css'
+})
+export class RecordatoriosDialogComponent {
+
+}
---
+++ src/app/cuu-casos/casos-information-dialog/recordatorios-caso/recordatorios-list/recordatorios-list.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-recordatorios-list',
+  standalone: true,
+  imports: [],
+  templateUrl: './recordatorios-list.component.html',
+  styleUrl: './recordatorios-list.component.css'
+})
+export class RecordatoriosListComponent {
+
+}
